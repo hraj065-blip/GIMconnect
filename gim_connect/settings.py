211d0fd9a133856ffe79c1fef3bb6df9391bb1db
harsh raj <hraj065@gmail.com>
2026-06-15 00:46:43 +0530

    Configure Cloudinary storage settings
    Add Cloudinary settings for user selfies storage

--- gim_connect/settings.py
+++ gim_connect/settings.py
@@ -143,6 +143,13 @@ STORAGES = {
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+# Cloudinary settings for saving user selfies
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
 # ---------------------------------------------------------------------------
 # Email Configuration
 # ---------------------------------------------------------------------------
